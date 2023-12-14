989f3aa13d8bb90a45c76631cbbdb2413542a023
amanbhagat41 <34489796+amanbhagat41@users.noreply.github.com>
2023-12-14 12:54:20 -0500

    NEW SOCKETS AYYAYA

--- client.py
+++ client.py
@@ -1,103 +1,41 @@
 import socket
 import os
-import json
-
 class ChatClient:
     def __init__(self, myEmail):
         self.HOST = '127.0.0.1'
-        self.PORT = 5555
-        self.MOTHERSHIP = 5557
-
-        self.client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.client_socket.connect((self.HOST, self.PORT))
-
+        self.PORT = 5552
+        self.FILEPORT = 5553
         self.command_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.command_socket.connect((self.HOST, self.MOTHERSHIP))
+        self.command_socket.connect((self.HOST, self.PORT))
+        self.command_socket.send(myEmail.encode('utf-8'))
 
-        self.client_socket.send(myEmail.encode('utf-8'))
+        self.file_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.file_socket.connect((self.HOST, self.FILEPORT))
+
+       
         self.myEmail = myEmail
-    
     def receive_file(self):
-        try:
-            with open('contact.json', 'r') as file:
-                contacts = json.load(file)
-            #Get the alert, contains the sender email
-            #We are gonna look up and see if the sender email is in contacts first
-
-            #if it is, ask if it wants it, if not discard
-            print("Expecting a file transfer.....")
-            ack = self.client_socket.recv(1024).decode('utf-8')
-            if ack == "incomingRequest":
-                print("Request inbound")
-                self.client_socket.send(f"sendSenderEmail".encode('utf-8'))
-                senderEmail = self.client_socket.recv(1024).decode('utf-8')
-                if is_email_a_contact(senderEmail, contacts):
-                    print("Checking if email is contact")
-                    self.client_socket.send(f'isContact'.encode('utf-8'))
-                    #Prompt user for incoming request
-                    isAccepted = input("Contact {senderEmail} is sending a file. Accept (y/n)?")
-                    if(isAccepted == 'y' or isAccepted == 'Y'):
-                        print("Accepted File")
-                        self.client_socket.send(f'accepted'.encode('utf-8'))
-                        ack = self.client_socket.recv(1024).decode('utf-8')
-                        if(ack == "acceptedSeen"):
-                            print("STARTING FILE TRANSFER")
-                            print("WIP")
-                    else:
-                        self.client_socket.send(f'rejected'.encode('utf-8'))
-                        print("Rejected File")
-
-                else:
-                    self.client_socket.send(f'notContact'.encode('utf-8'))
-                    print("Email not a contact")
-        except KeyboardInterrupt:
-            print("exiting....")
-            return
-        
-        
-                
-
-
-        
-        # folder_name = "download"
-        # file_name = input("Enter the name for the received file with extension: ")
-        # file_path = os.path.join(folder_name, file_name)
-
-        # # Create the download folder if it doesn't exist
-        # os.makedirs(folder_name, exist_ok=True)
-        
-
-        # print(f"File received and saved to {file_path}")
+        print("Waiting for File Request")
 
     def send_file(self, recipient_email, file_path):
-        try:
-            # Notify the server that a file will be sent
-            self.command_socket.send(f"send_user_file#{recipient_email}".encode('utf-8'))
-
-            # Wait for server's response to initiate file transfer
-            ack = self.command_socket.recv(1024).decode('utf-8')
-            if ack == "SendingInitated":
-                print("Server Acknowledge Handshake")
-                # Open the file and send its size first
-                file_size = os.path.getsize(file_path)
-                self.client_socket.send(str(file_size).encode('utf-8'))
-                ack = self.client_socket.recv(1024).decode('utf-8')
-                if(ack == "ReceivedFileSize"):
-                    print("Received File Size")
-                    #self.client_socket.send(f"{self.myEmail}".encode('utf-8'))
-                    self.client_socket.send(f'sendingInfo'.encode('utf-8'))
-                    ack = self.client_socket.recv(1024).decode('utf-8')
-                    if(ack == "sendEmail"):
-                        print("Sending Email to Server")
-                        self.client_socket.send(f'{self.myEmail}'.encode('utf-8'))
-                        ack = self.client_socket.recv(1024).decode('utf-8')
-                        if (ack == "ReceivedSenderEmail"):
-                            print("Received sender Email")
-                            self.client_socket.send(f'sendUserAlert'.encode('utf-8'))
-                        
-               
-        except Exception as e:
-            print(f"Error sending file: {e}")
+        print("WIP")
+        self.command_socket.send(b"send_user_file")
+        file_name = os.path.basename(file_path)
+        ack = self.file_socket.recv(1024).decode('utf-8')
+        if(ack == 'fileTransferReady'):
+            print("WE ARE READY")
+            self.file_socket.send(f'clientSideAcknowledge'.encode('utf-8'))
+            ack = self.file_socket.recv(1024).decode('utf-8')
+            if(ack == "initiate"):
+                print("KEEP GOING ITS WORKING!!!!")
+                #Send to Server--------------------
+                self.file_socket.send(f'sendingFileName'.encode('utf-8'))
+                ack = self.file_socket.recv(1024).decode('utf-8')
+                if(ack == "sendFileName"):
+                    self.file_socket.send(f'{file_name}'.encode('utf-8'))
+                    #file name
+                        #senders email
+                
 
     def getOnlineUsers(self):
         self.command_socket.send(b"get_online_users")
@@ -105,9 +43,5 @@ class ChatClient:
             response = self.command_socket.recv(1024).decode('utf-8')
         except Exception as e:
             print(f"Error receiving response: {e}")
-        return response.split(',')
-def is_email_a_contact(email, contacts):
-        for contact in contacts:
-            if contact['email'] == email:
-                return True
-        return False
+        users = response.split(',')
+        return users
--- server.py
+++ server.py
@@ -2,124 +2,78 @@ import socket
 import threading
 
 HOST = '127.0.0.1'
-PORT = 5555
-MOTHERSHIP = 5557
+PORT = 5552
+FILEPORT = 5553
 client_info = {}  # Dictionary to store client information (email: (socket, address))
-def handle_client(client_socket, address, listen, command_socket):
-    
-    print(f"Server Accepted connection from {address}")
 
-    email = command_socket.recv(1024).decode('utf-8')#
-    client_info[email] = (command_socket, address)#
+def handle_client(command_socket, address, file_socket, faddress):
+    print(f"Accepted connection from {address}")
+
+    email = command_socket.recv(1024).decode('utf-8')
+    client_info[email] = (command_socket, address)
 
     while True:
-        if(listen == True):
+        data = command_socket.recv(1024).decode('utf-8')
+        if not data:
+            break
+
+        print(f"Received from {address}: {data}")
+
+        #print(data)
+        # Check for the special command to get online users
+        split_result = data.split('#')
+        # Extract the second and third elements
+        #print(split_result)
+        selection = split_result[0]
+        if selection == "get_online_users":
+            online_users = [email for email in client_info]
+            online_users_str = ','.join(online_users)
+
+            try:
+                command_socket.send(f'{online_users_str}'.encode('utf-8'))
+            except Exception as e:
+                print(f"Error sending response to {address}: {e}")
+            return
+        elif selection == "send_user_file":
+            print("WIP")
+            file_socket_handler = threading.Thread(target=fileTransfer, args=(file_socket, faddress))
+            file_socket_handler.start()
             
-            data = command_socket.recv(1024).decode('utf-8')#
-            print("Command: {}".format(data))
-            if not data:
-                break
 
-            print(f"Received from {address}: {data}")
-
-            # Check for the special command to get online users
-            command, *args = data.split('#')
-            
-            if command == "get_online_users":
-                listen = False
-                handle_get_online_users(client_socket, address)
-                listen = True
-            elif command == "send_user_file":
-                listen = False
-                print("Command Sent")
-                handle_send_user_file(client_socket, args)
-                print("Im a pussy exiting when I shouldn't")
-
-    # Remove the client from the dictionary and close the connection
     del client_info[email]
-    client_socket.close()
     command_socket.close()
-
-def handle_get_online_users(client_socket, address):
-    try:
-        online_users = ','.join(client_info)
-        client_socket.send(online_users.encode('utf-8'))
-    except Exception as e:
-        print(f"Error sending response to {address}: {e}")
-
-def handle_send_user_file(client_socket, args):
-    if len(args) < 1:
-        return
-
-    recipient_email = args[0]
-    if recipient_email in client_info:
-        recipient_socket, _ = client_info[recipient_email]
-        try:
-            print("Sending has Initiated")
-            command_socket.send(f'SendingInitated'.encode('utf-8'))
-            file_size = int(client_socket.recv(1024).decode('utf-8'))
-            print("Server Received file size {}".format(file_size))
-            client_socket.send(f'ReceivedFileSize'.encode('utf-8'))
-            ack = client_socket.recv(1024).decode('utf-8')
-            if (ack == "sendingInfo"):
-                print("Sending Info")
-                client_socket.send(f'sendEmail'.encode('utf-8'))
-                senderEmail = client_socket.recv(1024).decode('utf-8')
-                print("Received Sender email {}".format(senderEmail))
-                client_socket.send(f'ReceivedSenderEmail'.encode('utf-8'))
-                ack = client_socket.recv(1024).decode('utf-8')
-                if(ack == "sendUserAlert"):
-                    print("Sending User Alert")
-                    #Send Recipient Alert
-                    recipient_socket.send(f'incomingRequest'.encode('utf-8'))
-                    ack = client_socket.recv(1024).decode('utf-8')
-                    if (ack == "sendSenderEmail"):
-                        print("sender email Received")
-                        recipient_socket.send(f'{senderEmail}'.encode('utf-8'))
-                        ack = client_socket.recv(1024).decode('utf-8')
-                        if(ack == "isContact"):
-                            #Continue
-                            print("User is a contact")
-                            ack = client_socket.recv(1024).decode('utf-8')
-                            if(ack == "accepted"):
-                                recipient_socket.send(f'acceptedSeen'.encode('utf-8'))
-                                print("Recipient Accepted Transfer")
-                                print("STARTING FILE TRANSFER")
-                                #Start FILE TRANSFER
-                            elif(ack == "rejected"):
-                                print("Recipient has rejected your transfer request")
-                            
-                        elif (ack == "notContact"):
-                            print("This user has not added you as a contact yet...")
-                            return
-            #recipient_socket.send(b"FileTransferComplete")
-        except Exception as e:
-            print(f"Error forwarding file to {recipient_email}: {e}")
-    else:
-        print(f"Recipient {recipient_email} not found.")
+def fileTransfer(file_socket, faddress):
+    print("I am being called for a file transfer")
+    print(f"file connection established from {faddress}")
+    file_socket.send(f'fileTransferReady'.encode('utf-8'))
+    ack = file_socket.recv(1024).decode('utf-8')
+    if(ack == 'clientSideAcknowledge'):
+        file_socket.send(f'initiate'.encode('utf-8'))
+        print("Are we working???")
+        ack = file_socket.recv(1024).decode('utf-8') #recieves sendingFileName
+        if(ack == 'sendingFileName'):
+            file_socket.send(f'sendFileName'.encode('utf-8'))
+            file_path = file_socket.recv(1024).decode('utf-8')
+            print(file_path)
 
 if __name__ == '__main__':
-    listen = True
     try:
         server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         server_socket.bind((HOST, PORT))
         server_socket.listen()
 
-        mothership_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        mothership_socket.bind((HOST, MOTHERSHIP))
-        mothership_socket.listen()
-
         print(f"Server is listening on {HOST}:{PORT}")
-        print(f"Mothership is listening on {HOST}:{MOTHERSHIP}")
 
+        file_command = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        file_command.bind((HOST, FILEPORT))
+        file_command.listen()
+                
         while True:
-            client_socket, address = server_socket.accept()
-            command_socket, faddress = mothership_socket.accept()
-
-            client_handler = threading.Thread(target=handle_client, args=(client_socket, address, listen,command_socket))
+            command_socket, address = server_socket.accept()
+            file_socket, faddress = file_command.accept()
+            client_handler = threading.Thread(target=handle_client, args=(command_socket, address, file_socket, faddress))
             client_handler.start()
     except KeyboardInterrupt:
         print("\nTurning Off Server.........")
     finally:
         server_socket.close()
-        mothership_socket.close()
